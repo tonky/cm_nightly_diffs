be90c27a161dcd802ac9d3db2ba897502937ef47
Igor Tonky <gtonky@gmail.com>
2011-02-02 08:33:26 +0000

    looks like working

--- cm_daily.py
+++ cm_daily.py
@@ -3,26 +3,29 @@
 import json
 import re
 from subprocess import Popen, PIPE
+import time
+
+last_run = time.time() - 60*60*24
 
 with open('projects.txt', 'r') as f:
     cm_p = [p.strip() for p in f.readlines()]
     f.closed
 
-# print cm_p
-
 merged = Popen(["ssh", "-p", "29418", "r.cyanogenmod.com", "gerrit", "query",
-    "status:merged", "limit:5", "age:1d", "--format=JSON"],
+    "status:merged", "limit:80", "--format=JSON"],
     stdout=PIPE).communicate()[0]
 
 for c in merged.strip().split("\n"):
     change = json.loads(c)
 
-    if change['project'] in cm_p:
-        print change['url'],
-        print change['project'],
-        print change['subject'],
-        print change['branch']
+    if 'lastUpdated' in change and change['lastUpdated'] < last_run:
+        continue
 
-    # for p in cm_p:
-        # if re.search('(?<=-)\w+', 'spam-egg'):
-    print "'%s'" % change
+    if 'project' in change and change['project'] in cm_p:
+        # print "=========="
+        print "%s | %s" % (change['project'], change['subject'])
+        # print time.ctime(change['lastUpdated'])
+        # print change['branch']
+        # print change['url']
+        print "%s: %s" % (time.ctime(change['lastUpdated']), change['url'])
+        print
